75692b64a9a17826fac4ba6ef9050cfc4136dcaf
Troy Tural <turalt@educ.college-montreal.qc.ca>
2022-09-09 16:19:37 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+chaine_1 = "I'm at"
+chaine_2 = " my absolute"
+chaine_3 = " limit!"
+
+chaine_4 = chaine_1 + chaine_2 + chaine_3  # La 4ieme chaine
+
+print(chaine_4)  #
+"I'm at my absolute limit!"
+
+print(len(chaine_4))  #
