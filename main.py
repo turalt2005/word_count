34e53a16f4eb40ea6079062ef44091abba51b4a1
turalt <turalt@educ.college-montreal.qc.ca>
2022-09-14 09:44:08 -0400

    TP1

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-chaine_1 = "I'm at"
-chaine_2 = " my absolute"
-chaine_3 = " limit!"
+def count_word(chaine):
+    print(len(chaine.split(" ")))  # Cette ligne va permettre au code de conter 1 mot chaque fois qu'ìl y a une espace vide dans la phrase
 
-chaine_4 = chaine_1 + chaine_2 + chaine_3  # La 4ieme chaine
 
-print(chaine_4)  #
-"I'm at my absolute limit!"
-
-print(len(chaine_4))  #
+phrase = input("Ecrit une phrase")  # Lorsque le code va etre executer, la console va nous demander d'ecrire une phrase grace au input
+count_word(phrase)  # Cette ligne va permetre au console de conter le nombre de mot dans la phrase ecrite
